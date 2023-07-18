15e75a07a0d4c2bde5a0cb4d04fb3761021f6fd3
Sukairo-02 <reka.serzh@yandex.ru>
2023-07-18 17:16:17 +0300

    Implemented language selector, minor fixes, moved header components to index (to be separated entirely...)

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,3 +1,30 @@
 import '@styles/index.scss'
 
-console.log('This shite finally works!')
+const langSelector = {
+	wrapper: document.getElementById('langSelector'),
+	text: document.getElementById('langSelector__text'),
+	arrow: document.getElementById('langSelector__arrow'),
+	popup: document.getElementById('langSelector__popup')
+}
+
+langSelector.wrapper?.addEventListener('click', () => {
+	langSelector.arrow?.classList.toggle('langSelector__arrow_selected')
+	langSelector.popup?.classList.toggle('langSelector__popup_enabled')
+})
+
+window.addEventListener('click', (event) => {
+	if (!(event.target instanceof HTMLElement)) return
+
+	const { target } = event
+	if (!target.matches('#langSelector')) {
+		const langSelector = {
+			wrapper: document.getElementById('langSelector'),
+			text: document.getElementById('langSelector__text'),
+			arrow: document.getElementById('langSelector__arrow'),
+			popup: document.getElementById('langSelector__popup')
+		}
+
+		langSelector.arrow?.classList.remove('langSelector__arrow_selected')
+		langSelector.popup?.classList.remove('langSelector__popup_enabled')
+	}
+})
