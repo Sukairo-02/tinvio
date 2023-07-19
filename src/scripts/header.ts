11e3df1496623d9eeefa9ff50329a3863e1cf2f7
Sukairo-02 <reka.serzh@yandex.ru>
2023-07-19 21:19:55 +0300

    Implemented modal window, separated header scripts, minor fixes\improvements

---
+++ src/scripts/header.ts
@@ -0,0 +1,48 @@
+import '@styles/components/header.scss'
+
+const langSelector = {
+	wrapper: document.getElementById('langSelector'),
+	text: document.getElementById('langSelector__text'),
+	arrow: document.getElementById('langSelector__arrow'),
+	popup: document.getElementById('langSelector__popup')
+}
+
+langSelector.wrapper?.addEventListener('click', () => {
+	langSelector.arrow?.classList.toggle('selected')
+	langSelector.popup?.classList.toggle('enabled')
+})
+
+window.addEventListener('click', (event) => {
+	if (!(event.target instanceof HTMLElement)) return
+
+	const { target } = event
+	if (!target.matches('#langSelector')) {
+		langSelector.arrow?.classList.remove('selected')
+		langSelector.popup?.classList.remove('enabled')
+	}
+})
+
+const header = document.querySelector('.header')
+const headerButton = document.querySelector('.header__button')
+const bodyWrapper = document.querySelector('.body__wrapper')
+
+document.addEventListener('scroll', () => {
+	if (bodyWrapper) {
+		if (window.scrollY == 0) {
+			header?.classList.remove('postScroll')
+			headerButton?.classList.remove('postScroll')
+		} else {
+			header?.classList.add('postScroll')
+			headerButton?.classList.add('postScroll')
+		}
+	}
+})
+
+const modalButton = document.getElementById('header__modalButton')
+const modalWindow = document.getElementById('header__mobileModal')
+
+modalButton?.addEventListener('click', () => {
+	modalButton.classList.toggle('activated')
+	modalWindow?.classList.toggle('enabled')
+	langSelector.wrapper?.classList.toggle('enabled')
+})
